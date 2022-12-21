6a0ed71e547636185f4fa6ecffe6ad3eaf121c18
Hunter Padgett <gizmokabanjo@gmail.com>
2022-12-21 16:42:13 -0500

    added mobile touch events for submit and delete btns

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const taskSpan = document.getElementsByClassName("taskSpan");
 const colorToggle = document.querySelector("#darkmode");
 const currentLink = document.getElementById("cssLink");
 const todos = JSON.parse(localStorage.getItem("todos")) || [];
-let darkMode = localStorage.getItem("darkMode")
-
-
+let darkMode = localStorage.getItem("darkMode");
 
 window.addEventListener("load", () => {
  form.addEventListener("submit", (e) => {
@@ -29,7 +27,7 @@ window.addEventListener("load", () => {
    done: false,
    createdAt: new Date().getTime(),
   };
-  
+
   todos.push(todo);
 
   localStorage.setItem("todos", JSON.stringify(todos));
@@ -39,16 +37,16 @@ window.addEventListener("load", () => {
   // growTextBox();
  });
 
-// checks to see if dark mode was enabled before reload and returns it to dark mode state if it was
-let currentLink = document.getElementById("cssLink");
+ // checks to see if dark mode was enabled before reload and returns it to dark mode state if it was
+ let currentLink = document.getElementById("cssLink");
 
  if (darkMode === "enabled") {
-    colorToggle.checked = true;
-    currentLink.href = "styles/darkMode.css";
-  } else {
-    colorToggle.checked = false;
-    currentLink.href = "styles/lightMode.css";
-  }
+  colorToggle.checked = true;
+  currentLink.href = "styles/darkMode.css";
+ } else {
+  colorToggle.checked = false;
+  currentLink.href = "styles/lightMode.css";
+ }
 
  displayTodos();
  getTime();
@@ -56,11 +54,10 @@ let currentLink = document.getElementById("cssLink");
 
 function displayTodos() {
  tasks.innerHTML = "";
-  
+
  todos.forEach((todo, index) => {
- 
   const todoIndex = todos.indexOf(todo);
-  
+
   return (tasks.innerHTML += `
       <div id="todo-item" class="">
      <div id="todo-content" class="">
@@ -82,78 +79,102 @@ function displayTodos() {
      </div>
     </div>
     `);
-  });
-
-  const editBtns = document.querySelectorAll("#edit");
-  const deleteBtns = document.querySelectorAll("#delete");
-  const check = document.querySelectorAll("#checked");
-  const taskInputs = document.querySelectorAll("#taskInput");
-  
-  // eventlisteners for checkboxes, edit buttons, and delete buttons
-
-  // This way, when the editTask, deleteTask, and checkbox functions are called, the correct      taskInput element will be passed as an argument and used in those functions.
-
-  editBtns.forEach((button) => {
-    button.addEventListener("click", () =>
-    editTask(button.parentElement.parentElement.querySelector("#taskInput")))
-  });
-
-  deleteBtns.forEach((button) => {
-    button.addEventListener("click", () => 
-    deleteTask(button.parentElement.parentElement.querySelector("#taskInput")))
-  });
-
-  check.forEach((button) => button.addEventListener("click", () => handleCheckboxClick(button.parentElement.parentElement.querySelector("#taskInput"), button)));
-
-  taskInputs.forEach((taskInput) => {
-    taskInput.addEventListener("input", (e) => {
-      // Use e.target instead of this to reference the input element
-      const input = e.target;
-      // Set the width of the #taskInput element based on its scrollWidth
-      input.style.width = input.scrollWidth + "px";
-      console.log(input.scrollWidth);
-    });
-  });
-
-  colorToggle.addEventListener('change', () => {
-    darkMode = localStorage.getItem("darkMode")
-    let currentLink = document.getElementById("cssLink");
-  // console.log("sdfsdfs")
-    if(colorToggle.checked) {
-      console.log(darkMode)
-      localStorage.setItem("darkMode", "enabled");
-      currentLink.href = "styles/darkMode.css";
-    } else {
-      console.log(darkMode)
-      localStorage.setItem("darkMode", null);
-      currentLink.href = "styles/lightMode.css";
-    }
  });
 
-  document.addEventListener('touchstart', handleTouch);
-  document.addEventListener('touchmove', handleTouch);
-  document.addEventListener('touchend', handleTouch);
+ const editBtns = document.querySelectorAll("#edit");
+ const deleteBtns = document.querySelectorAll("#delete");
+ const check = document.querySelectorAll("#checked");
+ const taskInputs = document.querySelectorAll("#taskInput");
+
+ // eventlisteners for checkboxes, edit buttons, and delete buttons
+
+ // This way, when the editTask, deleteTask, and checkbox functions are called, the correct      taskInput element will be passed as an argument and used in those functions.
+
+ editBtns.forEach((button) => {
+  button.addEventListener("click", () =>
+   editTask(button.parentElement.parentElement.querySelector("#taskInput"))
+  );
+ });
+
+ deleteBtns.forEach((button) => {
+  button.addEventListener("click", () =>
+   deleteTask(button.parentElement.parentElement.querySelector("#taskInput"))
+  );
+
+  // event listeners inside the the foreach of each delete btn
+  // that adds button "touch" events for mobile delete btn
+  button.addEventListener("touchstart", (e) => {
+   if (e.type === "touchstart") {
+    button.style.transform = "scale(0.85)";
+    button.style.transition = "0.1s ease-in-out";
+   }
+  });
+
+  button.addEventListener("touchend", (e) => {
+   if (e.type === "touchend") {
+    button.style.transform = "scale(1.0)";
+    button.style.transition = "0.1s ease-in-out";
+   }
+  });
+ });
+
+ check.forEach((button) =>
+  button.addEventListener("click", () =>
+   handleCheckboxClick(
+    button.parentElement.parentElement.querySelector("#taskInput"),
+    button
+   )
+  )
+ );
+
+ taskInputs.forEach((taskInput) => {
+  taskInput.addEventListener("input", (e) => {
+   // Use e.target instead of this to reference the input element
+   const input = e.target;
+   // Set the width of the #taskInput element based on its scrollWidth
+   input.style.width = input.scrollWidth + "px";
+   console.log(input.scrollWidth);
+  });
+ });
+
+ colorToggle.addEventListener("change", () => {
+  darkMode = localStorage.getItem("darkMode");
+  let currentLink = document.getElementById("cssLink");
+  // console.log("sdfsdfs")
+  if (colorToggle.checked) {
+   console.log(darkMode);
+   localStorage.setItem("darkMode", "enabled");
+   currentLink.href = "styles/darkMode.css";
+  } else {
+   console.log(darkMode);
+   localStorage.setItem("darkMode", null);
+   currentLink.href = "styles/lightMode.css";
+  }
+ });
+
+ submit.addEventListener("touchstart", handleTouch);
+ submit.addEventListener("touchmove", handleTouch);
+ submit.addEventListener("touchend", handleTouch);
 }
 
-
 function handleCheckboxClick(taskInput, checked) {
-  // Get the corresponding checked element
-  
-  console.log(checked)
+ // Get the corresponding checked element
 
-  // check if the checkbox is checked then apply necessary style
-  if (checked.checked) {
-    // Add the "done" class to the taskInput element if checked
-    taskInput.classList.add("done")
-  } else {
-    // remove the "done" class to the taskInput element if checked
-    taskInput.classList.remove("done")
-  }
+ console.log(checked);
+
+ // check if the checkbox is checked then apply necessary style
+ if (checked.checked) {
+  // Add the "done" class to the taskInput element if checked
+  taskInput.classList.add("done");
+ } else {
+  // remove the "done" class to the taskInput element if checked
+  taskInput.classList.remove("done");
+ }
 }
 
 function deleteTask(taskInput) {
-  // console.log(taskInput)
-  // Remove the to-do item from the DOM
+ // console.log(taskInput)
+ // Remove the to-do item from the DOM
  taskInput.parentElement.parentElement.parentElement.remove();
 
  // Get the index of the deleted to-do item from the dataset.index property
@@ -166,31 +187,29 @@ function deleteTask(taskInput) {
  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-
 function editTask(taskInput) {
- 
-  // make the text input editable and focus on the input
-  taskInput.removeAttribute("readonly");
-  taskInput.focus();
+ // make the text input editable and focus on the input
+ taskInput.removeAttribute("readonly");
+ taskInput.focus();
 
-  // Add an onblur event listener to the taskInput element
-  taskInput.addEventListener("blur", (e) => {
-    // Find the index of the edited to-do item in the todos array
-    const index = e.target.dataset.index;
+ // Add an onblur event listener to the taskInput element
+ taskInput.addEventListener("blur", (e) => {
+  // Find the index of the edited to-do item in the todos array
+  const index = e.target.dataset.index;
 
-    // Update the content of the to-do item in the todos array
-    todos[index].content = e.target.value;
+  // Update the content of the to-do item in the todos array
+  todos[index].content = e.target.value;
 
-    // Update the todos array in local storage
-    localStorage.setItem("todos", JSON.stringify(todos));
-  });
+  // Update the todos array in local storage
+  localStorage.setItem("todos", JSON.stringify(todos));
+ });
 }
 
 // function to display morning, afternoon evening
 function getTime() {
  let date = new Date();
  let time = date.getHours();
-//  console.log(time);
+ //  console.log(time);
 
  if (time < 12) {
   timeOfDay.innerHTML = "GOOD MORNING, ";
@@ -201,14 +220,15 @@ function getTime() {
  }
 }
 
+// function that adds button "touch" events for mobile submit btn
 function handleTouch(e) {
-  e.preventDefault();
-  if (e.type === 'touchstart') {
-    submit.style.transform = "scale(0.85)";
-    submit.style.transition = "0.1s ease-in-out"
-  }
-  if (e.type === 'touchend') {
-    submit.style.transform = "scale(1.0)"
-    submit.style.transition = "0.1s ease-in-out"
-  }
+//  e.preventDefault();
+ if (e.type === "touchstart") {
+  submit.style.transform = "scale(0.85)";
+  submit.style.transition = "0.1s ease-in-out";
+ }
+ if (e.type === "touchend") {
+  submit.style.transform = "scale(1.0)";
+  submit.style.transition = "0.1s ease-in-out";
+ }
 }
